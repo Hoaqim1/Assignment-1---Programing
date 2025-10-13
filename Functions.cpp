b0be981c60c09ef97568b0e3edc0a4731822a061
Elmin-Cr <elminc1@hotmail.com>
2025-10-13 14:21:53 +0200

    Added variance and standarddeviation, also changed it so values in the array can contain decimals.

--- Functions.cpp
+++ Functions.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <windows.h>
+#include <cmath>
 using namespace std;
 
-/*Denna funktion beräknar och visar statistik som summan, minsta, största och medelvärdet i en lista, dock så nekar den användaren om listan
-inte har några värden. Måste lägga in varians och standaravvikelser. WIP
+/*Denna funktion beräknar och visar statistik som summan, minsta, största, medelvärdet, varians och standardavvikelser i en lista, dock så nekar den användaren om listan
+inte har några värden. 
 */ 
 
-void Add_Value(int list[], int &amount){
+void Add_Value(double list[], int &amount){
     if (amount < 10) {
-        cout << "Skriv in ett värde(heltal): " << '\n';
+        cout << "Skriv in ett värde: " << '\n';
         cin >> list[amount]; //tar input och lägger in det i listan
         amount++;
 
@@ -19,15 +20,16 @@ void Add_Value(int list[], int &amount){
     }
 
 
-void Show_Value(int list[], int amount){
+void Show_Value(double list[], int amount){
         if (amount == 0){
             cout << "Listan har inga värden" << '\n';
             return;
         }
 
-        int sum = 0;
-        int Minval = list[0];
-        int Maxval = list[0];
+        //variabler för summa, minimum och maximum
+        double sum = 0;
+        double Minval = list[0];
+        double Maxval = list[0];
 
         for(int i = 0; i < amount; i++){
             sum += list[i];
@@ -35,9 +37,24 @@ void Show_Value(int list[], int amount){
             if(list[i] > Maxval) Maxval = list[i];
         }
 
+        //tar summan av hela listan och delar med antal värden för att få medelvärdet
+        double mdlv = sum /amount;
+        //beräkna variansen(https://www.youtube.com/watch?v=fYblNUZZqjA), hade väldigt svårt att förstå så utgick från denna video.
+        double var = 0;
+        for(int i = 0; i < amount; i++){
+            var += pow(list[i] - mdlv, 2);
+        }
+        var = var / amount; 
+
+        double stdav = sqrt(var); //standardavvikelsen är roten ur variansen
+
+
+
         cout << "Summan av värdena i arrayen är: " << sum << '\n';
         cout << "Minsta värdet i arrayen är: " << Minval << '\n';
         cout << "Största värdet i arrayen är: " << Maxval << '\n';
-        cout << "Medelvärdet av värdena i arrayen är: " << int(sum) / amount << '\n';
+        cout << "Medelvärdet av värdena i arrayen är: "  << mdlv << '\n';
+        cout << "Variansen är: " << var << '\n';
+        cout << "Standardavvikelsen är: " << stdav << '\n';
 
     }
